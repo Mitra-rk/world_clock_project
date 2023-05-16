9024664ab0cb25c980aa44604229652c1df195a7
Mitra-rk <119427404+Mitra-rk@users.noreply.github.com>
2023-05-16 12:50:57 +0330

    addded more code

--- js/index.js
+++ js/index.js
@@ -1,28 +1,50 @@
-function getCurrentTime(){
+function getCurrentTime() {
   //Los Angeles
+
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTz = moment().tz("America/Los_angeles");
-  losAngelesDateElement.innerHTML = losAngelesTz.format("MMMM Do YYYY ");
-  losAngelesTimeElement.innerHTML = losAngelesTz.format(
-    "h:mm:ss [<small>]A[</small>] "
-  );
-  
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTz = moment().tz("America/Los_angeles");
+    losAngelesDateElement.innerHTML = losAngelesTz.format("MMMM Do YYYY ");
+    losAngelesTimeElement.innerHTML = losAngelesTz.format(
+      "h:mm:ss [<small>]A[</small>] "
+    );
+  }
+
   //Paris
   let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
 
-  let parisTz = moment().tz("Europe / Paris");
-  parisDateElement.innerHTML = parisTz.format("MMMM Do YYYY ");
-  parisTimeElement.innerHTML = parisTz.format("h:mm:ss [<small>]A[</small>] ");
+    let parisTz = moment().tz("Europe / Paris");
+    parisDateElement.innerHTML = parisTz.format("MMMM Do YYYY ");
+    parisTimeElement.innerHTML = parisTz.format(
+      "h:mm:ss [<small>]A[</small>] "
+    );
+  }
 }
 
-
-setInterval(getCurrentTime,1000);
-
-
-
-
+function updateCity(event) {
+  let cityName = event.target.value;
+  let city1 = cityName.replace("_", " ").split("/")[1];
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city" >
+          <div>
+            <h2>${city1} 🚩</h2>
+            <div class="date">${moment()
+              .tz(cityName)
+              .format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${moment()
+            .tz(cityName)
+            .format("h:mm:ss [<small>]A[</small>] ")}</div>
+        </div>
+      </div>`;
+      
+}
+let cityElement = document.querySelector("#city");
+cityElement.addEventListener("change", updateCity);
+setInterval(getCurrentTime, 1000);
 
